4e8a2ee98eeb905e91cc8ae3f062cb6556b59144
m_farahmand <m.farahmand@outlook.com>
2022-11-20 16:27:14 +0330

    chore(): code formatting

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,61 +1,61 @@
 <template>
-	<div class="footer" :class="{ 'ltrdir': ($i18n.locale === 'en') }">
-		<div>
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-3">
-						<div class="address">
-							<img src="/assets/images/home/map.png" alt="map" />
-						</div>
-					</div>
-					<div class="col-sm-6">
-						<div class="companyinfo">
-							<h4>
-								<b>
-									&nbsp; {{ $t('aboutUs') }}
-								</b>
-							</h4>
-							<p>
-								&nbsp; &nbsp;{{ $t('title') }} می کوشد تا
-								بهترین خدمات رزرو هتل، بلیط هواپیما، بلیط چارتر و تور مسافرتی را در سراسر جهان با
-								بالاترین
-								استاندارد ها، به مشتریان خود ارائه دهد. خرید بلیط هواپیمای تور خارجی، خرید بلیط
-								هواپیمای
-								تور داخلی ، خرید بلیط هواپیمای تور لحظه آخری، خرید بلیط هواپیمای تور هوایی کربلا، خرید
-								بلیط هواپیما تور عتبات، بلیط استانبول، بلیط دبی، بلیط چارتر هواپیما ، بلیط ارزان
-								هواپیما
-								و رزرو هتل ارزان قیمت و لوکس از جمله رزرو هتل استانبول ، هتل دبی و هتل مشهد بلیط هواپیما
-								مشهد، بلیط کیش و … تور دبی، تور ترکیه ، تور استانبول ، تور آنتالیا ، تور مالزی ، تور
-								تایلند
-								<nuxt-link to="/About" active-class="active">
-									<b> بیشتر </b>
-								</nuxt-link>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+  <div class="footer" :class="{ ltrdir: $i18n.locale === 'en' }">
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="address">
+              <img src="/assets/images/home/map.png" alt="map" />
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="companyinfo">
+              <h4>
+                <b> &nbsp; {{ $t('aboutUs') }} </b>
+              </h4>
+              <p>
+                &nbsp; &nbsp;{{ $t('title') }} می کوشد تا بهترین خدمات رزرو هتل،
+                بلیط هواپیما، بلیط چارتر و تور مسافرتی را در سراسر جهان با
+                بالاترین استاندارد ها، به مشتریان خود ارائه دهد. خرید بلیط
+                هواپیمای تور خارجی، خرید بلیط هواپیمای تور داخلی ، خرید بلیط
+                هواپیمای تور لحظه آخری، خرید بلیط هواپیمای تور هوایی کربلا، خرید
+                بلیط هواپیما تور عتبات، بلیط استانبول، بلیط دبی، بلیط چارتر
+                هواپیما ، بلیط ارزان هواپیما و رزرو هتل ارزان قیمت و لوکس از
+                جمله رزرو هتل استانبول ، هتل دبی و هتل مشهد بلیط هواپیما مشهد،
+                بلیط کیش و … تور دبی، تور ترکیه ، تور استانبول ، تور آنتالیا ،
+                تور مالزی ، تور تایلند
+                <nuxt-link to="/About" active-class="active">
+                  <b> بیشتر </b>
+                </nuxt-link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-		<div class="footer-bottom">
-			<div class="container">
-				<div class="row">
-					<p>Copyright © 2022
-						<a target="_blank" href="http://www.tourine.ir/"><b>{{ $t('title') }}</b></a>. All rights reserved.
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <p>
+            Copyright © 2022
+            <a target="_blank" href="/"
+              ><b>{{ $t('title') }}</b></a
+            >. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	name: "Footer",
-	computed: {
-		availableLocales() {
-			return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
-		}
-	}
-}
+  name: 'Footer',
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale);
+    },
+  },
+};
 </script>
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,111 +1,109 @@
-
-const getRequest = async (url: string) => await (globalThis as any).$nuxt.$http.$get(`${process.env.baseUrl}${url}`);
+const getRequest = async (url: string) =>
+  await (globalThis as any).$nuxt.$http.$get(`${url}`);
 
 // `http://localhost:1337${url}`
 
 function toEnglishNumber(strNum: string) {
-    var pn = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-    var en = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var pn = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+  var en = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    var cache = strNum;
-    for (var i = 0; i < 10; i++) {
-        var regex_fa = new RegExp(pn[i], 'g');
-        cache = cache.replace(regex_fa, en[i]);
-    }
-    return cache;
+  var cache = strNum;
+  for (var i = 0; i < 10; i++) {
+    var regex_fa = new RegExp(pn[i], 'g');
+    cache = cache.replace(regex_fa, en[i]);
+  }
+  return cache;
 }
 
 function getMonthName(strDate: string) {
-    let strDay = strDate.substring(strDate.indexOf("/") + 1, strDate.length);
+  let strDay = strDate.substring(strDate.indexOf('/') + 1, strDate.length);
 
-    let strMonth = strDate.substring(0, strDate.indexOf("/"));
+  let strMonth = strDate.substring(0, strDate.indexOf('/'));
 
-    switch (strMonth) {
-        case "۱":
-            strMonth = "فروردین"
-            break;
-        case "۲":
-            strMonth = "اردیبهشت"
-            break;
-        case "۳":
-            strMonth = "خرداد"
-            break;
-        case "۴":
-            strMonth = "تیر"
-            break;
-        case "۵":
-            strMonth = "مرداد"
-            break;
-        case "۶":
-            strMonth = "شهریور"
-            break;
-        case "۷":
-            strMonth = "مهر"
-            break;
-        case "۸":
-            strMonth = "آبان"
-            break;
-        case "۹":
-            strMonth = "آذر"
-            break;
-        case "۱۰":
-            strMonth = "دی"
-            break;
-        case "۱۱":
-            strMonth = "بهمن"
-            break;
-        case "۱۲":
-            strMonth = "اسفند"
-            break;
-        default:
-        // code block
-    }
+  switch (strMonth) {
+    case '۱':
+      strMonth = 'فروردین';
+      break;
+    case '۲':
+      strMonth = 'اردیبهشت';
+      break;
+    case '۳':
+      strMonth = 'خرداد';
+      break;
+    case '۴':
+      strMonth = 'تیر';
+      break;
+    case '۵':
+      strMonth = 'مرداد';
+      break;
+    case '۶':
+      strMonth = 'شهریور';
+      break;
+    case '۷':
+      strMonth = 'مهر';
+      break;
+    case '۸':
+      strMonth = 'آبان';
+      break;
+    case '۹':
+      strMonth = 'آذر';
+      break;
+    case '۱۰':
+      strMonth = 'دی';
+      break;
+    case '۱۱':
+      strMonth = 'بهمن';
+      break;
+    case '۱۲':
+      strMonth = 'اسفند';
+      break;
+    default:
+    // code block
+  }
 
-    return strDay + " " + strMonth;
+  return strDay + ' ' + strMonth;
 }
 
 function dateDiff(from: string, to: string) {
+  let result = 0;
 
-    let result = 0;
+  let dateFrom = toEnglishNumber(
+    new Date(from.replaceAll('-', '/')).toLocaleDateString('fa-IR')
+  );
+  let dateTo = toEnglishNumber(
+    new Date(to.replaceAll('-', '/')).toLocaleDateString('fa-IR')
+  );
 
-    let dateFrom = toEnglishNumber(new Date(from.replaceAll("-", "/")).toLocaleDateString('fa-IR'));
-    let dateTo = toEnglishNumber(new Date(to.replaceAll("-", "/")).toLocaleDateString('fa-IR'));
+  // let dateFrom = "1401/2/29";
+  // let dateTo = "1401/3/4";
 
-    // let dateFrom = "1401/2/29";
-    // let dateTo = "1401/3/4";
+  let yFrom = parseInt(dateFrom.substring(0, 4));
+  let mFrom = parseInt(dateFrom.substring(5, dateFrom.indexOf('/', 6)));
+  let dFrom = parseInt(
+    dateFrom.substring(dateFrom.indexOf('/', 6) + 1, dateFrom.length)
+  );
 
-    let yFrom = parseInt(dateFrom.substring(0, 4));
-    let mFrom = parseInt(dateFrom.substring(5, dateFrom.indexOf("/", 6)));
-    let dFrom = parseInt(dateFrom.substring(dateFrom.indexOf("/", 6) + 1, dateFrom.length));
+  let yTo = parseInt(dateTo.substring(0, 4));
+  let mTo = parseInt(dateTo.substring(5, dateTo.indexOf('/', 6)));
+  let dTo = parseInt(
+    dateTo.substring(dateTo.indexOf('/', 6) + 1, dateTo.length)
+  );
 
-    let yTo = parseInt(dateTo.substring(0, 4));
-    let mTo = parseInt(dateTo.substring(5, dateTo.indexOf("/", 6)));
-    let dTo = parseInt(dateTo.substring(dateTo.indexOf("/", 6) + 1, dateTo.length));
+  if (yFrom == yTo) {
+    if (mFrom == mTo) {
+      if (dFrom < dTo) {
+        result = dTo - dFrom;
+        // debugger;
+      }
+    } else if (mFrom == 12) result = 29 - dFrom + dTo;
+    else if (mFrom < mTo)
+      if (mFrom < 7) result = 31 - dFrom + dTo;
+      else result = 30 - dFrom + dTo;
+  }
 
-    if (yFrom == yTo) {
-        if (mFrom == mTo) {
-            if (dFrom < dTo) {
-                result = dTo - dFrom;
-                // debugger;
-            }
-        }
-        else if (mFrom == 12)
-            result = (29 - dFrom) + dTo;
-        else if (mFrom < mTo)
-            if (mFrom < 7)
-                result = (31 - dFrom) + dTo;
-            else
-                result = (30 - dFrom) + dTo;
-    }
+  // debugger
 
-    // debugger
-
-    return result;
+  return result;
 }
 
-export {
-    getRequest,
-    dateDiff,
-    getMonthName,
-
-}
+export { getRequest, dateDiff, getMonthName };
